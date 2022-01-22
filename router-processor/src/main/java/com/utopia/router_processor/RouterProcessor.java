e7f4af0b4f66ba84b8b0baf954b827e4ea64b302
yupenglei <yupengniaosanshi@gmail.com>
2022-01-22 22:23:00 +0800

    实现路由映射文件生成

--- router-processor/src/main/java/com/utopia/router_processor/RouterProcessor.java
+++ router-processor/src/main/java/com/utopia/router_processor/RouterProcessor.java
@@ -1,7 +1,12 @@
 package com.utopia.router_processor;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
@@ -23,9 +28,37 @@ public class RouterProcessor extends AbstractProcessor {
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
     log("process() called with: annotations = [" + annotations + "], roundEnv = [" + roundEnv + "]");
     if (roundEnv.processingOver()) {
+      log("处理流程已完成");
       return false;
     }
+    Set<String> annotationSet = annotations.stream().map(it -> it.getQualifiedName().toString()).collect(Collectors.toSet());
+    String annotationFullName = Destination.class.getCanonicalName();
+    if (!annotationSet.contains(annotationFullName)) {
+      log("注解列表中没有" + annotationFullName);
+      return false;
+    }
+
     Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Destination.class);
+    if (elements.isEmpty()) {
+      log("没有被" + annotationFullName + "注解的类");
+      return false;
+    }
+    log(">>>> APT开始处理");
+
+    String pkg = "com.utopia.mapping";
+    String clzName = "RouterMapping_" + System.currentTimeMillis();
+    String fullName = pkg + "." + clzName;
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("package ").append(pkg).append(";\n ")
+        .append("\n")
+        .append("import java.util.HashMap;\n")
+        .append("import java.util.Map;\n")
+        .append("\n")
+        .append("public class ").append(clzName).append(" {\n")
+        .append("  public static Map<String, String> getMapping() {\n")
+        .append("    Map<String, String> mapping = new HashMap<>();\n");
+
     for (Element elem : elements) {
       if (!(elem instanceof TypeElement)) {
         error(elem.getKind().name() + " element is not TypeElement");
@@ -39,18 +72,29 @@ public class RouterProcessor extends AbstractProcessor {
         continue;
       }
 
+      // 获取注解信息
       String url = annotation.url();
       String description = annotation.description();
       String realPath = typeElement.getQualifiedName().toString();
 
-      log("url=" + url);
-      log("description=" + description);
-      log("realPath=" + realPath);
+      String line = "    mapping.put(\"" + url + "\", \"" + realPath + "\");\n";
+      builder.append(line);
+    }
+    builder.append("    return mapping;\n")
+        .append("  }\n")
+        .append("}");
 
+    // 开始生成java文件
+    try (OutputStream outputStream = processingEnv.getFiler().createSourceFile(fullName).openOutputStream();
+         Writer writer = new OutputStreamWriter(outputStream)) {
+      writer.write(builder.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
+    log(">>>> 处理结束");
 
-    return false;
+    return true;
   }
 
   private void log(String msg) {
