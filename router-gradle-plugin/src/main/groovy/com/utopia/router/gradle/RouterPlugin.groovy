f10483afeba816b724f0a84b647e749abc582cb7
yupenglei <yupengniaosanshi@gmail.com>
2022-01-23 20:19:32 +0800

    通过plugin给kapt传参；javac之后生成mapping的md文件

--- router-gradle-plugin/src/main/groovy/com/utopia/router/gradle/RouterPlugin.groovy
+++ router-gradle-plugin/src/main/groovy/com/utopia/router/gradle/RouterPlugin.groovy
@@ -1,5 +1,6 @@
 package com.utopia.router.gradle
 
+import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,11 +9,80 @@ class RouterPlugin implements Plugin<Project> {
     void apply(Project project) {
         println "RouterPlugin: apply from ${project.name}"
 
+        def rootProjectDir = project.rootProject.projectDir
+        // 1. 自动帮用户传递路径参数到注解处理器中
+        def kapt = project.extensions.findByName("kapt")
+        if (kapt != null) {
+            kapt.arguments {
+                arg("root_project_dir", rootProjectDir.absolutePath)
+            }
+        }
+        // 2. 实现旧的构建产物的自动清理
+        project.clean.doFirst {
+            def routerMappingDir = new File(rootProjectDir, "router_mapping")
+            if (routerMappingDir.exists()) {
+                routerMappingDir.deleteDir()
+            }
+        }
+
         project.extensions.create("router", RouterExtension)
 
         project.afterEvaluate {
-            RouterExtension extension = project["router"]
-            println "用户设置的wikiDir:${extension.wikiDir}"
+            println "it.type=${it.class}"
+            stepMarkdown(it)
+        }
+    }
+
+    private static void stepMarkdown(Project project) {
+        def rootProjectDir = project.rootProject.projectDir
+        RouterExtension extension = project["router"]
+        println "用户设置的wikiDir:${extension.wikiDir}"
+
+        // 3. javac任务后，汇总生成文档
+        project.tasks.findAll {
+            it.name.startsWith("compile") && it.name.endsWith("JavaWithJavac")
+        }.each {
+            println "filtered task: $it.name"
+            it.doLast {
+                File routerMappingDir = new File(rootProjectDir, "router_mapping")
+                if (!routerMappingDir.exists()) {
+                    return
+                }
+                def childFiles = routerMappingDir.listFiles()
+                if (childFiles.length < 1) {
+                    return
+                }
+
+                StringBuilder markdownBuilder = new StringBuilder()
+                markdownBuilder.append("# 页面文档\n\n")
+
+                childFiles.each {
+                    if (it.name.endsWith(".json")) {
+                        JsonSlurper jsonSlurper = new JsonSlurper()
+                        def content = jsonSlurper.parse(it)
+                        content.each {
+                            def url = it['url']
+                            def description = it['description']
+                            def realPath = it['realPath']
+                            markdownBuilder.append("## $description\n")
+                                    .append("- url:$url\n")
+                                    .append("- realPath:$realPath\n")
+                        }
+                    }
+                }
+
+                def wikiDir = new File(extension.wikiDir)
+                if (!wikiDir.exists()) {
+                    wikiDir.mkdir()
+                }
+                def wikiFile = new File(wikiDir, "页面文档.md")
+                if (wikiFile.exists()) {
+                    wikiFile.delete()
+                }
+
+                println "生成页文档"
+                wikiFile.write(markdownBuilder.toString())
+            }
         }
     }
 }
